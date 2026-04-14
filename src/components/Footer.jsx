cef1d731124f8d326f6439d26f7c522b699f5952
32SignatureSmilez <32signaturesmilezweb@gmail.com>
2026-04-14 20:46:43 +0530

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -115,6 +115,13 @@ const Footer = () => {
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
             </svg>
           </a>
+          <a href="https://www.instagram.com/signaturesmilezcliniq?igsh=MW5sbjUzc2tvNmRpZw==" target="_blank" rel="noreferrer" className="social-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#3b4657" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+            </svg>
+          </a>
         </div>
       </div>
 
